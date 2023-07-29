1964ead4243820f2ddd02f2d25f34af12bc6b0dd
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2023-07-29 13:47:14 +0200

    port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'process';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(PORT, '0.0.0.0');
 }
 bootstrap();
