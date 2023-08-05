9d75f5715a74b7d3385be34d1aabec022f9585b1
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2023-08-05 17:05:22 +0200

    product module init

---
+++ src/product/dto/create-product.dto.ts
@@ -0,0 +1,13 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateProductDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  price: number;
+
+  description?: string;
+}
